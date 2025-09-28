a98a5aded6bad580b39d76d53dd4ac243f20fa48
Md Shahriar Forhad <89806405+Shahriar88@users.noreply.github.com>
2025-09-28 11:53:16 -0500

    Update URDFLoader.js

--- urdf_js/URDFLoader.js
+++ urdf_js/URDFLoader.js
@@ -1,8 +1,10 @@
 /*import * as THREE from 'three';*/
 
 import * as THREE from 'https://unpkg.com/three@0.150.1/build/three.module.js';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
+import { STLLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/STLLoader.js';
+import { ColladaLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/ColladaLoader.js';
+//import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+//import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
 import { URDFRobot, URDFJoint, URDFLink, URDFCollider, URDFVisual, URDFMimicJoint } from './URDFClasses.js';
 
 /*
