8b2dfc2f2ea6b96c13dbc326f7d636be35028dea
Md Shahriar Forhad <89806405+Shahriar88@users.noreply.github.com>
2025-09-27 23:45:44 -0500

    Update URDFLoader.js

--- js_urdf/URDFLoader.js
+++ js_urdf/URDFLoader.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
+import * as THREE from './three.js';
+//import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+//import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
 import { URDFRobot, URDFJoint, URDFLink, URDFCollider, URDFVisual, URDFMimicJoint } from './URDFClasses.js';
 
 /*
